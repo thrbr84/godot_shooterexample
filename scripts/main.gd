4e4d8961b4ff464d3631d5a6a136a0009dca5636
Thiago Bruno <thiago.bruno@geracaoweb.com>
2020-01-21 00:22:16 -0300

    base

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,11 +4,8 @@ var totalBoxShot = 0
 
 func _on_boxSpawn_timeout():
 	if $person.dir != Vector2.ZERO:
-		var gpos = $person.global_position
-		var boxPos = gpos * randf() * (PI * 2) * 1
-		
 		var box = load("res://scenes/box.tscn").instance()
-		box.global_position = boxPos
+		box.global_position = $person/body/boxDrop.global_position
 		box.connect("explode", self, "_on_box_shot")
 		add_child(box)
 
