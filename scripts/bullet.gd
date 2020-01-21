fe6556b5c499f1e96fa3e8a393d00f2fb182f2e3
Thiago Bruno <thiago.bruno@geracaoweb.com>
2020-01-20 21:14:19 -0300

    base

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -7,14 +7,11 @@ func _ready():
 	if direction == Vector2.ZERO:
 		direction.x = 300
 		
-	prints(direction)
-
 func _process(delta):
-	position += direction * velocidade * delta
+	position += (direction.normalized()*300) * velocidade * delta
 
 func _on_visibility_screen_exited():
 	queue_free()
-
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("box"):
